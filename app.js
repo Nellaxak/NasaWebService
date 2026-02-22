98429f6dc8bcf0be47a28895cf95e64b9641b54b
Nellaxak <Nellaxak@yandex.ru>
2026-02-22 17:41:35 +0300

    post page10

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ async function CalcData(page) {
 //let startDate:'string'
 //let page = 0
 app.get('/page:page', async (_req, res) => {
-  console.log('render page', _req.params.page)
-  const startDate =  _req.params.page
+  console.log('render page', _req.query.page)
+  const startDate =  _req.query.page
   const resp = await fetch(`https://api.nasa.gov/neo/rest/v1/feed?start_date=${startDate}&end_date=${startDate}&api_key=3wa5hHgFuqhf6XiefvqzkcDQWZ01aOOK4vNZEXsP`);
   const data = await resp.json()
   res.send(data);
