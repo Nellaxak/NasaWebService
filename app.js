1a22291587b2a33c2f81292f81ec8de4025f5815
Nellaxak <Nellaxak@yandex.ru>
2026-02-22 17:27:21 +0300

    post page8

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ async function CalcData(page) {
 //let startDate:'string'
 let page = 0
 app.get('/', async (_req, res) => {
+  console.log('render page', page)
   const startDate = await CalcData(page)
   const resp = await fetch(`https://api.nasa.gov/neo/rest/v1/feed?start_date=${startDate}&end_date=${startDate}&api_key=3wa5hHgFuqhf6XiefvqzkcDQWZ01aOOK4vNZEXsP`);
   const data = await resp.json()
