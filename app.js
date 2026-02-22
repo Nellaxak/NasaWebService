1e4935d968e9ad19774a2d7584a3d7640cf86933
Nellaxak <Nellaxak@yandex.ru>
2026-02-22 16:14:30 +0300

    post page1

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ import express from 'express';
 
 const app = express()
 const port = process.env.PORT || 3000;
-async function CalcData() {
+async function CalcData(page) {
   let currentDate = new Date()
-  currentDate.setDate(currentDate.getDate())// + this.count);
+  currentDate.setDate(currentDate.getDate() + page);
   let tomorrow = new Date();
-  tomorrow.setDate(tomorrow.getDate())// + this.count);
+  tomorrow.setDate(tomorrow.getDate() + page);
   //console.log('myDate', currentDate, endNext)
   let startDate = currentDate.getFullYear() + '-' +
     (currentDate.getMonth() + 1) + '-' +
@@ -14,16 +14,23 @@ async function CalcData() {
   return new Promise((resolve) => {
     resolve(startDate)
   })
-  //return { startDate, endDate }
 }
 //let startDate:'string'
-const startDate = await CalcData()
-console.log(startDate)
 app.get('/', async (_req, res) => {
+  const startDate = await CalcData(0)
   const resp = await fetch(`https://api.nasa.gov/neo/rest/v1/feed?start_date=${startDate}&end_date=${startDate}&api_key=3wa5hHgFuqhf6XiefvqzkcDQWZ01aOOK4vNZEXsP`);
   const data = await resp.json()
   res.send(data);
 })
+app.post('/page', async (req, res) => {
+  console.log('Received data:', req.body); // Access the submitted data
+  const startDate = await CalcData(Number(req.body.page))
+  const resp = await fetch(`https://api.nasa.gov/neo/rest/v1/feed?start_date=${startDate}&end_date=${startDate}&api_key=3wa5hHgFuqhf6XiefvqzkcDQWZ01aOOK4vNZEXsP`);
+  const data = await resp.json()
+  // You can then save this data to a database, perform validation, etc.
+  // Send a response back to the client
+  res.status(201).send(data);
+});
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
