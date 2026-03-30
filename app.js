94a3a6b71316acca40af4efa3cd4cd9bff2c341f
Nellaxak <Nellaxak@yandex.ru>
2026-03-30 11:53:12 +0300

    first commit69

--- app.js
+++ app.js
@@ -15,12 +15,11 @@ let statusMap = new Map()
 let differenceSet = new Set()
 app.get('/page/:page', async (_req, res) => {
   console.log('render page', _req.params)
-  const page = _req.params.page
-  //https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=DEMO_KEY
-  const resp = await fetch(`https://api.nasa.gov/neo/rest/v1/neo/browse?page=${page}&size=8&api_key=3wa5hHgFuqhf6XiefvqzkcDQWZ01aOOK4vNZEXsP`);
+  const startDate = _req.params.page
+  const resp = await fetch(`https://api.nasa.gov/neo/rest/v1/feed?start_date=${startDate}&end_date=${startDate}&detailed=true&api_key=3wa5hHgFuqhf6XiefvqzkcDQWZ01aOOK4vNZEXsP`);
   const data = await resp.json()
-  console.log('status',resp.status)
-  //res.set('count-Header', data.element_count);
+  console.log('count', data.element_count, resp.status)
+  res.set('count-Header', data.element_count);
   res.send(data)
 })
 app.get('/api/id/:id', async (_req, res) => {
