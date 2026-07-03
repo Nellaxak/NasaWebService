406957a8b03c765ecf68729cadfdabb8e1fe8255
Nellaxak <134867203+Nellaxak@users.noreply.github.com>
2026-07-03 11:48:11 +0300

    Update app.js
    4

--- app.js
+++ app.js
@@ -31,13 +31,13 @@ app.get('/page/:page', async (_req, res) => {
   console.log('dataPrev',dataPrev)
   const dataNext=await respNext.json()
   const dataSelf=await respSelf.json()
-  const near_earth_objectsPrev=dataPrev.near_earth_objects[0]
+  const near_earth_objectsPrev=dataPrev.near_earth_objects
   console.log('near_earth_objectsPrev',near_earth_objectsPrev)
   const near_earth_objectsNext=dataNext.near_earth_objects[0]
   const near_earth_objectsSelf=dataSelf.near_earth_objects[0]
-  allArr.push(...near_earth_objectsPrev)
+  /*allArr.push(...near_earth_objectsPrev)
   allArr.push(...near_earth_objectsSelf)
-  allArr.push(...near_earth_objectsNext)
+  allArr.push(...near_earth_objectsNext)*/
   console.log('count', data.element_count, resp.status)
   res.send({data:allArr,startItem: dataPrev.element_count})
 })
