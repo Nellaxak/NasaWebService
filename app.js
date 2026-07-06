6a6a4d7cd634507cf853455727e8d86aa1d958af
Nellaxak <134867203+Nellaxak@users.noreply.github.com>
2026-07-06 21:06:22 +0300

    Update app.js
    edede

--- app.js
+++ app.js
@@ -54,6 +54,7 @@ console.log(mskDate.toLocaleString('ru-RU', options));
     let startDate = currentDate.getFullYear() + '-' +
         (currentDate.getMonth() + 1) + '-' +
         currentDate.getDate();
+    console.log('startDate',startDate)
     return startDate
 }
 const app = express()
