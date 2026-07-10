8ec5f2a7e1144355ab754eadc02b296d055b1a78
Nellaxak <134867203+Nellaxak@users.noreply.github.com>
2026-07-10 10:28:08 +0300

    Update app.js
    cvcvcv;;;;

--- app.js
+++ app.js
@@ -89,8 +89,9 @@ app.get('/page/:page', async (_req, res) => {
   const near_earth_objectsNext=Object.values(dataNext.near_earth_objects)
   const near_earth_objectsSelf=Object.values(dataSelf.near_earth_objects)
     if (Number(_req.params.page)>0){
-        console.log('page>0',_req.params.page,Object.keys(dataPrev.near_earth_objects))
          const dataPrev=await respPrev.json()
+        console.log('page>0',_req.params.page,Object.keys(dataPrev.near_earth_objects))
+        
         const near_earth_objectsPrev=Object.values(dataPrev.near_earth_objects)
   allArr.push(...near_earth_objectsPrev) 
     }
